7ce5ecc828d0720fbbcdab4c83b96e7014759f0c
Koray YILMAZ <kyilmaz80@gmail.com>
2019-12-29 17:51:32 +0300

    modified file for demo

--- examples/httpparser_from_file.py
+++ examples/httpparser_from_file.py
@@ -5,12 +5,14 @@ Created on 2012-3-24
 @author: fengclient
 '''
 from http_parser.pyparser import HttpParser
+from http_parser.util import b
 
 if __name__ == '__main__':
-    rsp = open('d:\\172_response.txt').read()
+    rsp = open('./resp.txt').read()
     # if your are reading a text file from windows, u may need manually convert \n to \r\n
     # universal newline support: http://docs.python.org/library/functions.html#open
     rsp = rsp.replace('\n', '\r\n')
+    rsp = b(rsp)
     p = HttpParser()
     p.execute(rsp, len(rsp))
     print(p.get_headers())
